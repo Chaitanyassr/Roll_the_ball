ea92988ec24b1e2930ecd00d35501c4895030df2
Chaitanya <chaitanyas0101@gmail.com>
2020-01-09 09:56:15 +0530

    fix code error

--- New Game Project/KinematicBody.gd
+++ New Game Project/KinematicBody.gd
@@ -7,11 +7,12 @@ func _ready():
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 5
-		if Input.is_action_pressed("ui_left"):
+			velocity.x = 5
+	elif Input.is_action_pressed("ui_left"):
 			velocity.x = -5
-		if Input.is_action_pressed("ui_up"):
+			
+	if Input.is_action_pressed("ui_up"):
 			velocity.z = -5
-		if Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("ui_down"):
 			velocity.z = 5
-		move_and_slide(velocity)
+	move_and_slide(velocity)
