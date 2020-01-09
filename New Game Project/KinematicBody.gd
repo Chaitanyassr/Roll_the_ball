22760b1f206e319614a16a7ee92e0e6676707e59
Chaitanya <chaitanyas0101@gmail.com>
2020-01-09 10:07:27 +0530

    fix few code

--- New Game Project/KinematicBody.gd
+++ New Game Project/KinematicBody.gd
@@ -13,6 +13,8 @@ func _physics_process(delta):
 			velocity.x = 5
 	elif Input.is_action_pressed("ui_left"):
 			velocity.x = -5
+	else:
+		velocity.x = lerp(velocity.x,0,0.1)
 			
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.x = 0
@@ -20,4 +22,6 @@ func _physics_process(delta):
 			velocity.z = -5
 	elif Input.is_action_pressed("ui_down"):
 			velocity.z = 5
+	else:
+		velocity.z = lerp(velocity.z,0,0.1)
 	move_and_slide(velocity)
