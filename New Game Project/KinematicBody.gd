074aff26046b5c66b6408a091df8b96e53cdef32
Chaitanya <chaitanyas0101@gmail.com>
2020-01-09 10:00:56 +0530

    added mement to ball

--- New Game Project/KinematicBody.gd
+++ New Game Project/KinematicBody.gd
@@ -6,11 +6,16 @@ func _ready():
 	pass 
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
+	
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
 			velocity.x = 5
 	elif Input.is_action_pressed("ui_left"):
 			velocity.x = -5
 			
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
 	if Input.is_action_pressed("ui_up"):
 			velocity.z = -5
 	elif Input.is_action_pressed("ui_down"):
